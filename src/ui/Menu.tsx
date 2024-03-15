dceb5e2db5aa30d9541f18f90fb85dadd2cc4abc
kazmonroy <katherinemonroy@gmail.com>
2024-03-15 17:23:11 +0100

    Update Menu component and add main cabin row actions

--- src/ui/Menu.tsx
+++ src/ui/Menu.tsx
@@ -62,7 +62,6 @@ function Toggle({ id }: { id: number }) {
       x: window.innerWidth - rect?.width - rect?.x,
       y: rect?.y + rect?.height + 8,
     });
-    console.log(rect);
 
     openId === null || openId !== id ? open(id) : close();
   };
