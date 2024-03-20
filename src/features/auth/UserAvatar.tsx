221051130a5fc15f98bae6e032740be67b1fd693
kazmonroy <katherinemonroy@gmail.com>
2024-03-20 20:52:00 +0100

    Add user meta data in user avatar component

--- src/features/auth/UserAvatar.tsx
+++ src/features/auth/UserAvatar.tsx
@@ -1,9 +1,18 @@
+import { useUser } from './hooks/useUser';
 import styles from './styles/UserAvatar.module.css';
 
 function UserAvatar() {
+  const { user } = useUser();
+  console.log(user?.user_metadata.fullName);
+  const avatarUrl = user?.user_metadata.avatar;
+
   return (
     <div className={styles.avatarWrapper}>
-      <img src='' alt='' className={styles.avatar} />
+      <img
+        src={avatarUrl || 'https://www.svgrepo.com/show/457777/user-alt.svg'}
+        alt='User avatar'
+        className={styles.avatar}
+      />
     </div>
   );
 }
