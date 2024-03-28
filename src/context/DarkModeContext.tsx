45e899e5fd0bd68f1162d24471de813769ea0150
kazmonroy <katherinemonroy@gmail.com>
2024-03-29 00:18:27 +0100

    Add dark mode context

---
+++ src/context/DarkModeContext.tsx
@@ -0,0 +1,37 @@
+import { createContext, useContext } from 'react';
+import { useLocalStorageState } from '../hooks/useLocalStorage';
+
+interface Context {
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+}
+
+const DarkModeContext = createContext<Context>({
+  isDarkMode: false,
+  toggleDarkMode: () => {},
+});
+
+function DarkModeProvider({ children }: { children: JSX.Element }) {
+  const initialState = { initialState: false, key: 'isDarkMode' };
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(initialState);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((isDark: boolean) => !isDark);
+  };
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (context === undefined) {
+    throw new Error('DarkModeContext was used outside of DarkModeProvider');
+  }
+
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };
--- src/ui/DarkModeToggle.tsx
+++ src/ui/DarkModeToggle.tsx
@@ -1,5 +1,6 @@
 import { HiOutlineMoon } from 'react-icons/hi2';
 import styles from './styles/DarkModeToggle.module.css';
+
 function DarkModeToggle() {
   return (
     <div className={styles.toggle}>
