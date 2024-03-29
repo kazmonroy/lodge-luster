458da83599f6e3d76448ee15753571bc598c0cfd
kazmonroy <katherinemonroy@gmail.com>
2024-03-29 23:53:25 +0100

    Clean duration chart and add prepareData as helped function

--- src/features/dashboard/DurationChart.tsx
+++ src/features/dashboard/DurationChart.tsx
@@ -9,12 +9,7 @@ import {
 import { Booking } from '../../services/types/collection';
 import styles from './styles/DurationChart.module.css';
 import { useDarkMode } from '../../context/DarkModeContext';
-
-interface StartData {
-  duration: string;
-  value: number;
-  color: string;
-}
+import { prepareData } from '../../utils/helpers';
 
 const startDataLight = [
   {
@@ -101,34 +96,6 @@ const startDataDark = [
     color: '#7e22ce',
   },
 ];
-
-function prepareData(startData: StartData[], stays: Booking[] = []) {
-  console.log(stays);
-  function incArrayValue(arr: StartData[], field: string) {
-    return arr.map((obj) =>
-      obj.duration === field ? { ...obj, value: obj.value + 1 } : obj
-    );
-  }
-
-  const data = stays
-    .reduce((arr, cur) => {
-      const num = cur.numNights;
-
-      if (num === 1) return incArrayValue(arr, '1 night');
-      if (num === 2) return incArrayValue(arr, '2 nights');
-      if (num === 3) return incArrayValue(arr, '3 nights');
-      if ([4, 5].includes(num)) return incArrayValue(arr, '4-5 nights');
-      if ([6, 7].includes(num)) return incArrayValue(arr, '6-7 nights');
-      if (num >= 8 && num <= 14) return incArrayValue(arr, '8-14 nights');
-      if (num >= 15 && num <= 21) return incArrayValue(arr, '15-21 nights');
-      if (num >= 21) return incArrayValue(arr, '21+ nights');
-      return arr;
-    }, startData)
-    .filter((obj) => obj.value > 0);
-  console.log(data);
-
-  return data;
-}
 
 function DurationChart({ confirmedStays }: { confirmedStays: Booking[] }) {
   const { isDarkMode } = useDarkMode();
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,10 +1,10 @@
 import { formatDistance, parseISO, differenceInDays } from 'date-fns';
 
 // We want to make this function work for both Date objects and strings (which come from Supabase)
-export const subtractDates = (dateStr1:string, dateStr2:string) =>
+export const subtractDates = (dateStr1: string, dateStr2: string) =>
   differenceInDays(parseISO(String(dateStr1)), parseISO(String(dateStr2)));
 
-export const formatDistanceFromNow = (dateStr:string) =>
+export const formatDistanceFromNow = (dateStr: string) =>
   formatDistance(parseISO(dateStr), new Date(), {
     addSuffix: true,
   })
@@ -23,7 +23,41 @@ export const getToday = function (options = {}) {
   return today.toISOString();
 };
 
-export const formatCurrency = (value:number) =>
+export const formatCurrency = (value: number) =>
   new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(
     value
   );
+
+import { Booking } from '../../../services/types/collection';
+
+interface StartData {
+  duration: string;
+  value: number;
+  color: string;
+}
+
+export function prepareData(startData: StartData[], stays: Booking[] = []) {
+  function incArrayValue(arr: StartData[], field: string) {
+    return arr.map((obj) =>
+      obj.duration === field ? { ...obj, value: obj.value + 1 } : obj
+    );
+  }
+
+  const data = stays
+    .reduce((arr, cur) => {
+      const num = cur.numNights;
+
+      if (num === 1) return incArrayValue(arr, '1 night');
+      if (num === 2) return incArrayValue(arr, '2 nights');
+      if (num === 3) return incArrayValue(arr, '3 nights');
+      if ([4, 5].includes(num!)) return incArrayValue(arr, '4-5 nights');
+      if ([6, 7].includes(num!)) return incArrayValue(arr, '6-7 nights');
+      if (num! >= 8 && num! <= 14) return incArrayValue(arr, '8-14 nights');
+      if (num! >= 15 && num! <= 21) return incArrayValue(arr, '15-21 nights');
+      if (num! >= 21) return incArrayValue(arr, '21+ nights');
+      return arr;
+    }, startData)
+    .filter((obj) => obj.value > 0);
+
+  return data;
+}
